d652296354b5ac1c46b9dc13e14436b68a40c07c
Hunter Stevens <onebree@gmail.com>
2017-02-24 13:11:33 -0500

    Exercise 19 finished

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -25,6 +25,19 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+
+    // mess with them
+    // pixels = redEffect(pixels);
+    //pixels = rgbSplit(pixels);
+    //ctx.globalAlpha = 0.8;
+    pixels = greenScreen(pixels);
+
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
+
   }, 16);
 }
 
@@ -41,6 +54,53 @@ function takePhoto() {
   link.setAttribute('download', 'handsome');
   link.innerHTML = `<img src=${data} alt="Handsome" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i]     = pixels.data[i] + 100;      // red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;   // green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;  // blue
+  }
+
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i];      // red
+    pixels.data[i + 500] = pixels.data[i + 1];  // green
+    pixels.data[i - 550] = pixels.data[i + 2];  // blue
+  }
+
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i += 4) {
+    red   = pixels.data[i];
+    green = pixels.data[i + 1];
+    blue  = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+      // remove pixels within the range
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
 }
 
 getVideo();
